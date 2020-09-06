a093a1e05eeb0a47e9e3d59ff0093f19c4b646d9
dantmnf <dantmnf@users.noreply.github.com>
2020-09-06 18:17:22 +0800

    update nuspec

--- Update-Nuspec.ps1
+++ Update-Nuspec.ps1
@@ -6,5 +6,7 @@ param (
 )
 $revision = git rev-list --count --first-parent HEAD
 $nuspec = [xml](Get-Content $infile)
-$nuspec.package.metadata.version += "+r$revision"
+$nuspec.package.metadata.version += ".$revision"
+$nuspec.package.metadata.repository.branch = git rev-parse --abbrev-ref HEAD
+$nuspec.package.metadata.repository.commit = git rev-parse --verify HEAD
 $nuspec.Save($outfile)
